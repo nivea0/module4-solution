0246376013ae103c0e373588ce4ea9f3b1539a6d
nivea0 <ibrahim.main101@gmail.com>
2022-05-01 00:21:02 +0400

    f1

---
+++ SpeakGoodBye.js
@@ -0,0 +1,9 @@
+var byeSpeaker = {};
+
+(function () {   
+    var speakWord = "Good Bye";   
+    byeSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    } 
+}
+)();
---
+++ SpeakHello.js
@@ -0,0 +1,9 @@
+var helloSpeaker = {};
+
+(function () { 
+    var speakWord = "Hello";    
+    helloSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }   
+}
+)();
